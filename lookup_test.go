ce000a1ee491eba69d8fd7b80fa9ab5f06e3bbe2
bughou <bughou@gmail.com>
2020-05-25 09:37:34 +0800

    save

--- lookup_test.go
+++ lookup_test.go
@@ -7,6 +7,7 @@ import (
 func ExampleNode_Lookup() {
 	root, _ := New("/", 0)
 	root.Add("/users", 1)
+	root.Add("/users/123/([0-9]+)", 22)
 	root.Add("/users/([0-9]+)", 2)
 	root.Add("/unix/([a-z]+)/([0-9.]+)", 3)
 	root.Add("/users/root", 4)
@@ -14,6 +15,7 @@ func ExampleNode_Lookup() {
 	fmt.Println(root.Lookup("/"))
 	fmt.Println(root.Lookup("/users"))
 	fmt.Println(root.Lookup("/users/123"))
+	fmt.Println(root.Lookup("/users/123/456"))
 	fmt.Println(root.Lookup("/unix/linux/4.4.0"))
 	fmt.Println(root.Lookup("/users/root"))
 	fmt.Println(root.Lookup("/987"))
@@ -25,6 +27,7 @@ func ExampleNode_Lookup() {
 	// 0 []
 	// 1 []
 	// 2 [123]
+	// 22 [456]
 	// 3 [linux 4.4.0]
 	// 4 []
 	// 5 [987]
